6e266de832751e05c29a2579905c4400ceeeca4d
moxdev <moxdev1@gmail.com>
2017-05-30 11:48:06 -0400

    footer finished

--- footer.php
+++ footer.php
@@ -25,6 +25,33 @@
         } ?>
 
       </div><!-- footer-message -->
+
+      <?php if ( function_exists( 'get_field' ) ) {
+
+        $name    = get_field( 'company_name', 'option' );
+        $address = get_field( 'address', 'option' );
+        $city    = get_field( 'city', 'option' );
+        $state   = get_field( 'state', 'option' );
+        $zip     = get_field( 'zip', 'option' );
+        $phone   = get_field( 'phone', 'option' );
+        $fax     = get_field( 'fax', 'option' ); ?>
+
+        <div class="contact-information">
+          <div itemscope itemtype="http://schema.org/Organization">
+            <span itemprop="name" class="company-name"><?php echo esc_html( $name ); ?></span>
+            <div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+                <span itemprop="streetAddress"><?php echo esc_html( $address ); ?>, </span>
+                <span itemprop="addressLocality"><?php echo esc_html( $city ); ?>, </span>
+                <span itemprop="addressRegion"><?php echo esc_html( $state ); ?></span>
+                <span itemprop="postalCode"><?php echo esc_html( $zip ); ?></span>
+            </div>
+              P: <span itemprop="telephone"><a href="tel:<?php echo esc_html( $phone ); ?>"><?php echo esc_html( $phone ); ?></a></span>&nbsp;
+              F: <span itemprop="faxNumber"><?php echo esc_html( $fax ); ?></span>
+          </div>
+        </div>
+
+      <?php } ?>
+
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
